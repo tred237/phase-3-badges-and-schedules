d7ab28353b04e3c0f544d78a13a3ddaea61564d8
trevor dewindt <trevor.dewindt@gmail.com>
2023-02-22 20:31:12 -0700

    commit tree 9fcd2878575dd969988abca8a1b9abb6944431d3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map{|i| badge_maker i}
+end
+
+def assign_rooms(guests)
+    assignments = []
+    guests.each_with_index{|v,i| assignments[i] = "Hello, #{v}! You'll be assigned to room #{i + 1}!"}
+    assignments
+end
+
+def printer guests
+    guests.map{|i| puts badge_maker i}
+    assign_rooms(guests).map{|i| puts i}
+end
